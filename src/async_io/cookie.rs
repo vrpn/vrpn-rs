5a2ab3fbe36a10ae7172ce07d9efe9d186711417
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-09-17 13:04:09 -0500

    async cookie fixing

--- src/async_io/cookie.rs
+++ src/async_io/cookie.rs
@@ -5,94 +5,78 @@
 use crate::{
     constants::{FILE_MAGIC_DATA, MAGIC_DATA},
     cookie::{check_ver_file_compatible, check_ver_nonfile_compatible},
-    prelude::{BytesMutExtras, WrappedConstantSize},
+    prelude::BytesMutExtras,
     ConstantBufferSize, CookieData, Error, Unbuffer,
 };
 use bytes::{Bytes, BytesMut};
-use futures::Future;
+use tokio::io::AsyncReadExt;
+
 use tokio::prelude::*;
-use tokio::io::{self, AsyncReadExt};
 
 /// Writes the supplied cookie to a stream.
-///
-/// Future resolves to the provided stream on success.
-async fn write_cookie<T>(stream: T, cookie: CookieData) -> Result<Item = T, Error = Error>
+async fn write_cookie<T>(stream: &mut T, cookie: CookieData) -> Result<(), Error>
 where
-    T: AsyncWrite,
+    T: AsyncWrite + Unpin,
 {
-    let buf = BytesMut::new().allocate_and_buffer(cookie)?;
-    stream
-        .write_all(stream, buf.freeze())
-        .map(|(stream, _)| stream)
-        .await
+    let buf = BytesMut::new().allocate_and_buffer(cookie)?.freeze();
+    stream.write_all(&buf).await?;
+    Ok(())
 }
 
 /// Reads a cookie's worth of data into a temporary buffer.
 ///
 /// Future resolves to (stream, buffer) on success.
-async fn read_cookie<T>(stream: T) -> Result<Item = (T, Vec<u8>), Error = Error>
+async fn read_cookie<T>(stream: &mut T) -> Result<Vec<u8>, Error>
 where
-    T: AsyncRead,
+    T: AsyncRead + Unpin,
 {
-    stream.read_exact(vec![0u8; CookieData::constant_buffer_size()]).from_err()
+    let mut buf = Vec::with_capacity(CookieData::constant_buffer_size());
+    stream.read_exact(&mut buf).await?;
+    Ok(buf)
 }
 
-fn verify_version_nonfile(msg: CookieData) -> impl Future<Item = (), Error = Error> {
-    check_ver_nonfile_compatible(msg.version).into_future()
-}
-
-fn verify_version_file(msg: CookieData) -> impl Future<Item = (), Error = Error> {
-    check_ver_file_compatible(msg.version).into_future()
-}
-
-// /// Writes the "non-file" magic cookie to the stream.
+/// Writes the "non-file" magic cookie to the stream.
 ///
 /// Future resolves to the provided stream on success.
-pub(crate) fn send_nonfile_cookie<T>(stream: T) -> impl Future<Item = T, Error = Error>
+pub(crate) async fn send_nonfile_cookie<T>(stream: &mut T) -> Result<(), Error>
 where
-    T: AsyncWrite,
+    T: AsyncWrite + Unpin,
 {
-    write_cookie(stream, CookieData::from(MAGIC_DATA))
+    write_cookie(stream, CookieData::from(MAGIC_DATA)).await
 }
 
 /// Writes the "file" magic cookie to the stream.
 ///
 /// Future resolves to the provided stream on success.
-pub(crate) fn send_file_cookie<T>(stream: T) -> impl Future<Item = T, Error = Error>
+pub(crate) async fn send_file_cookie<T>(stream: &mut T) -> Result<(), Error>
 where
-    T: AsyncWrite,
+    T: AsyncWrite + Unpin,
 {
-    write_cookie(stream, CookieData::from(FILE_MAGIC_DATA))
+    write_cookie(stream, CookieData::from(FILE_MAGIC_DATA)).await
+}
+
+/// Reads a cookie's worth of data from the stream, and cheacks to make sure it is the right version.
+pub(crate) async fn read_and_check_nonfile_cookie<T>(stream: &mut T) -> Result<(), Error>
+where
+    T: AsyncRead + Unpin,
+{
+    let read_buf: Vec<u8> = read_cookie(stream).await?;
+    let mut buf = Bytes::from(read_buf);
+    let msg = CookieData::unbuffer_ref(&mut buf)?;
+    check_ver_nonfile_compatible(msg.version)?;
+    Ok(())
 }
 
 /// Reads a cookie's worth of data from the stream, and cheacks to make sure it is the right version.
 ///
 /// Future resolves to the provided stream on success.
-pub(crate) fn read_and_check_nonfile_cookie<T>(stream: T) -> impl Future<Item = T, Error = Error>
+pub(crate) async fn read_and_check_file_cookie<T>(stream: &mut T) -> Result<(), Error>
 where
-    T: AsyncRead,
+    T: AsyncRead + Unpin,
 {
-    read_cookie(stream).and_then(|(stream, read_buf)| {
-        let mut buf = Bytes::from(&read_buf[..]);
-        CookieData::unbuffer_ref(&mut buf)
-            .into_future()
-            .and_then(verify_version_nonfile)
-            .and_then(|()| Ok(stream))
-    })
-}
-
-/// Reads a cookie's worth of data from the stream, and cheacks to make sure it is the right version.
-///
-/// Future resolves to the provided stream on success.
-pub(crate) async fn read_and_check_file_cookie<T>(stream: T) -> Result<Item = T, Error = Error>
-where
-    T: AsyncRead,
-{
-    read_cookie(stream).and_then(|(stream, read_buf)| {
-        let mut buf = Bytes::from(&read_buf[..]);
-        CookieData::unbuffer_ref(&mut buf)
-            .into_future()
-            .and_then(verify_version_file)
-            .and_then(|()| Ok(stream))
-    })
+    let read_buf: Vec<u8> = read_cookie(stream).await?;
+    let mut buf = Bytes::from(read_buf);
+    let msg = CookieData::unbuffer_ref(&mut buf)?;
+    check_ver_file_compatible(msg.version)?;
+    Ok(())
 }
