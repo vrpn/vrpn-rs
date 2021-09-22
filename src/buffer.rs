b3d72b810e819415d689cf3d090a6247ee61f66b
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-09-22 11:19:24 -0500

    Remove defunct extension trait and improve docs for buffer.

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,41 +1,29 @@
-// Copyright 2018, Collabora, Ltd.
+// Copyright 2018-2021, Collabora, Ltd.
 // SPDX-License-Identifier: BSL-1.0
 // Author: Ryan A. Pavlik <ryan.pavlik@collabora.com>
+
+//! Extension traits related to buffering types.
 
 use crate::{BufferSize, EmptyResult, Result, WrappedConstantSize};
 use bytes::{BufMut, BytesMut};
 
-pub trait BufMutExtras
-where
-    Self: Sized,
-{
-    /// Serialize the value to the buffer, without changing the allocated size.
-    fn buffer<T: Buffer>(self, v: T) -> Result<Self>;
-}
-
-impl<U> BufMutExtras for U
-where
-    U: BufMut + Sized,
-{
-    fn buffer<T: Buffer>(self, v: T) -> Result<Self> {
-        let mut buf = self;
-        v.buffer_ref(&mut buf)?;
-        Ok(buf)
-    }
-}
-
+/// Extension trait for BytesMut for easier interaction with stuff we can buffer.
 pub trait BytesMutExtras
 where
     Self: Sized,
 {
     /// Allocate enough space in the buffer for the given value, then serialize the value to the buffer.
+    ///
+    /// # Errors
+    /// If buffering fails.
     fn allocate_and_buffer<T: Buffer>(self, v: T) -> Result<Self>;
 }
 
 impl BytesMutExtras for BytesMut {
     fn allocate_and_buffer<T: Buffer>(mut self, v: T) -> Result<Self> {
         self.reserve(v.buffer_size());
-        self.buffer(v)
+        v.buffer_ref(&mut self)?;
+        Ok(self)
     }
 }
 
