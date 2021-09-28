a381768b3864fd4852361bc02b82d0cfca7e9909
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-09-28 13:34:14 -0500

    fix cookie reading and add tests

--- src/vrpn_async_std/cookie.rs
+++ src/vrpn_async_std/cookie.rs
@@ -4,7 +4,10 @@
 
 use crate::{
     buffer_unbuffer::{BytesMutExtras, ConstantBufferSize, UnbufferFrom},
-    data_types::cookie::{check_ver_file_compatible, check_ver_nonfile_compatible, CookieData},
+    data_types::{
+        constants::COOKIE_SIZE,
+        cookie::{check_ver_file_compatible, check_ver_nonfile_compatible, CookieData},
+    },
     VrpnError,
 };
 use async_std::prelude::*;
@@ -28,9 +31,9 @@ async fn read_cookie<T>(stream: &mut T) -> Result<Vec<u8>, VrpnError>
 where
     T: AsyncRead + Unpin,
 {
-    let mut buf = Vec::with_capacity(CookieData::constant_buffer_size());
+    let mut buf = [0u8; COOKIE_SIZE];
     stream.read_exact(&mut buf).await?;
-    Ok(buf)
+    Ok(buf.to_vec())
 }
 
 /// Writes the "non-file" magic cookie to the stream.
@@ -71,4 +74,76 @@ where
     let msg = CookieData::unbuffer_from(&mut buf)?;
     check_ver_file_compatible(msg.version)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        buffer_unbuffer::{BytesMutExtras, ConstantBufferSize},
+        data_types::{constants::COOKIE_SIZE, CookieData},
+    };
+    use async_std::task;
+    use bytes::{Bytes, BytesMut};
+    use futures::io::Cursor;
+
+    fn get_cookie_buf(file_cookie: bool) -> Bytes {
+        assert_eq!(CookieData::constant_buffer_size(), COOKIE_SIZE);
+        BytesMut::allocate_and_buffer(if file_cookie {
+            CookieData::make_file_cookie()
+        } else {
+            CookieData::make_cookie()
+        })
+        .expect("should buffer cookie")
+        .freeze()
+    }
+
+    #[test]
+    fn read_cookie() {
+        {
+            let cookie = get_cookie_buf(false);
+            let mut reader = Cursor::new(&cookie[..]);
+            let read_buf = task::block_on(super::read_cookie(&mut reader)).unwrap();
+            assert_eq!(CookieData::constant_buffer_size(), read_buf.len());
+            assert_eq!(&cookie[..], &read_buf[..]);
+        }
+        {
+            let cookie = get_cookie_buf(true);
+            let mut reader = Cursor::new(&cookie[..]);
+            let read_buf = task::block_on(super::read_cookie(&mut reader)).unwrap();
+            assert_eq!(CookieData::constant_buffer_size(), read_buf.len());
+            assert_eq!(&cookie[..], &read_buf[..]);
+        }
+    }
+
+    #[test]
+    fn check_cookie() {
+        {
+            let cookie = get_cookie_buf(false);
+            let mut reader = Cursor::new(&cookie[..]);
+            task::block_on(super::read_and_check_nonfile_cookie(&mut reader))
+                .expect("checking cookie should pass");
+        }
+        {
+            let cookie = get_cookie_buf(true);
+            let mut reader = Cursor::new(&cookie[..]);
+            task::block_on(super::read_and_check_file_cookie(&mut reader))
+                .expect("checking cookie should pass");
+        }
+    }
+
+    #[test]
+    fn write_cookie() {
+        {
+            let mut writer = Cursor::new(vec![0u8; COOKIE_SIZE]);
+            task::block_on(super::send_nonfile_cookie(&mut writer)).unwrap();
+            let write_buf = writer.into_inner();
+            assert_eq!(&get_cookie_buf(false), &write_buf);
+        }
+        {
+            let mut writer = Cursor::new(vec![0u8; COOKIE_SIZE]);
+            task::block_on(super::send_file_cookie(&mut writer)).unwrap();
+            let write_buf = writer.into_inner();
+            assert_eq!(&get_cookie_buf(true), &write_buf);
+        }
+    }
 }
